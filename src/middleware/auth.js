59971b2feed5f21171ac8a57d17ed77741bed80e
Gufran <iamgufran@outlook.com>
2023-06-12 21:43:23 +0530

    auth fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     // Validate the token
-    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // If valid, find the user by id and token property. Remember, `decoded has _id property`
     const user = await User.findOne({_id: decoded._id, "tokens.token": token});
